10d79ed521479855e8c48800d4626fadb8741f77
Sudo Platform Engineering <sudoplatform-engineering@anonyome.com>
2021-04-23 04:59:32 +0000

    Release 4.1.0

---
+++ src/types/types.ts
@@ -0,0 +1,40 @@
+/**
+ * Generic type for paginated results from list queries
+ */
+export interface ListOutput<T> {
+  items: T[]
+  nextToken?: string
+}
+
+/**
+ * Cache policy that determines how data is accessed when performing a query method
+ * from the Email Service.
+ */
+export enum CachePolicy {
+  // Use the device cached data.
+  CacheOnly,
+  // Query and use the data on the server.
+  RemoteOnly,
+}
+
+export interface Owner {
+  id: string
+  issuer: string
+}
+
+/**
+ * A filter to use on string fields when listing items in a repository
+ */
+export interface StringFilter {
+  ne?: string
+  eq?: string
+  beginsWith?: string
+}
+
+/**
+ * A filter to use on string fields when listing items in a repository
+ */
+export interface BooleanFilter {
+  eq?: boolean
+  ne?: boolean
+}
