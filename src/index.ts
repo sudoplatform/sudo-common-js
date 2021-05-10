d39c94576904dd5e21ca1e80be5c7b1e411a5f2e
Sudo Platform Engineering <sudoplatform-engineering@anonyome.com>
2021-05-10 04:03:08 +0000

    Release 5.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './configurationManager/defaultConfigurationManager'
 export * from './sudoKeyManager/sudoCryptoProvider'
 export * from './sudoKeyManager/sudoKeyManager'
+export * from './sudoKeyManager/publicKey'
 export * from './utils/base64'
 export * from './utils/buffer'
 export * from './errors/error'
