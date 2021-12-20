edd8c85e6ba48b41df349389708dd81712f4db74
Sudo Platform Engineering <sudoplatform-engineering@anonyome.com>
2021-12-20 04:40:26 +0000

    Release 6.1.0

--- bin/outdated-with-suppression.sh
+++ bin/outdated-with-suppression.sh
@@ -1,17 +1,94 @@
 #!/bin/sh
 
-yarn outdated --json | jq -s -c 'map(select(.type == "table").data.body) | .[] | unique_by(.[0]) | .[] | .[0]' | (new=""; while read package_name ; do
-  suppression=$(jq ".outdatedSuppressions[$package_name] | select (. != null)" package.json)
-  if [ -z "$suppression" ]; then
-    echo "New outdated_package ${package_name}:"
-    echo "${package_name}" | jq .
+function version_diff_type() {
+  v1="$1"
+  v2="$2"
+  v1_major=$(echo $v1 | sed -E -e 's/^([^.]+)\..*/\1/')
+  v2_major=$(echo $v2 | sed -E -e 's/^([^.]+)\..*/\1/')
+  v1_minor=$(echo $v1 | sed -E -e 's/^([^.]+)\.([^.]+)\..*/\2/')
+  v2_minor=$(echo $v2 | sed -E -e 's/^([^.]+)\.([^.]+)\..*/\2/')
+  if [ "${v1_major}" != "${v2_major}" ]; then echo 'major'; return 0; fi
+  if [ "${v1_minor}" != "${v2_minor}" ]; then echo 'minor'; return 0; fi
+  if [ "${v1}" != "${v2}" ]; then echo 'patch'; return 0; fi
+  echo 'equal'
+  return 0
+}
+
+function suppression_from_diff_type() {
+  diff_type="$1"
+  majorSuppression="$2"
+  minorSuppression="$3"
+  patchSuppression="$4"
+
+  case "${diff_type}" in
+    major)
+      if [ -n "${majorSuppression}" ]; then
+        echo "major ${majorSuppression}"
+      elif [ -n "${minorSuppression}" ]; then
+        echo "minor ${minorSuppression}"
+      elif [ -n "${patchSuppression}" ]; then
+        echo "patch ${patchSuppression}"
+      fi
+      ;;
+    minor)
+      if [ -n "${minorSuppression}" ]; then
+        echo "minor ${minorSuppression}"
+      elif [ -n "${patchSuppression}" ]; then
+        echo "patch ${patchSuppression}"
+      fi
+      ;;
+    patch)
+      if [ -n "${patchSuppression}" ]; then
+        echo "patch ${patchSuppression}"
+      fi
+      ;;
+  esac
+}
+
+yarn outdated --json | jq -r -s -c 'map(select(.type == "table").data.body) | .[] | unique_by(.[0]) | .[] | .[0] + " " + .[1] + " " + .[2] + " " + .[3]' | (new=""; while read package_name current wanted latest; do
+  majorSuppression=$(jq ".outdatedSuppressions.major[\"${package_name}\"] | select (. != null)" package.json)
+  minorSuppression=$(jq ".outdatedSuppressions.minor[\"${package_name}\"] | select (. != null)" package.json)
+  patchSuppression=$(jq ".outdatedSuppressions[\"${package_name}\"] | select (. != null)" package.json)
+  latest_diff_type=$(version_diff_type "${current}" "${latest}")
+  wanted_diff_type=$(version_diff_type "${current}" "${wanted}")
+
+  set -- $(suppression_from_diff_type "${latest_diff_type}" "${majorSuppression}" "${minorSuppression}" "${patchSuppression}")
+  latest_suppression_type="$1"
+  latest_suppression="$2"
+
+  set -- $(suppression_from_diff_type "${wanted_diff_type}" "${majorSuppression}" "${minorSuppression}" "${patchSuppression}")
+  wanted_suppression_type="$1"
+  wanted_suppression="$2"
+
+  # If package has major update but isn't suppre
+  suppression=""
+  suppression_type=""
+  outdated_diff_type="${latest_diff_type}"
+  if [ -n "${latest_suppression_type}" ]; then
+    if [ "${wanted_diff_type}" != "equal" ]; then
+      outdated_diff_type="${wanted_diff_type}"
+      if [ -n "${wanted_suppression_type}" ]; then
+        suppression_type="${wanted_suppression_type}"
+        suppression="${wanted_suppression}"
+      else
+        # Only suppressed at major level and we have minor or patch
+        # updates available - do not suppress
+        :
+      fi
+    else
+      suppression_type="${latest_suppression_type}"
+      suppression="${latest_suppression}"
+    fi
+  fi
+
+  if [ -z "${suppression}" ]; then
+    echo "${package_name}: current: ${current} wanted: ${wanted} latest: ${latest} [${outdated_diff_type} outdated]"
     new=1
-  elif [ $(date '+%s') -gt "$suppression" ]; then
-    echo "Suppression for outdated_package ${package_name} has expired. Please revisit:"
-    echo "${package_name}" | jq .
+  elif [ $(date '+%s') -gt "${suppression}" ]; then
+    echo "${package_name}: current: ${current} wanted: ${wanted} latest: ${latest} [${outdated_diff_type} suppression expired]"
     new=1
   else
-    echo "outdated_package ${package_name} is suppressed"
+    echo "${package_name}: current: ${current} wanted: ${wanted} latest: ${latest} [${outdated_diff_type} suppressed]"
   fi
 done
 if [ -n "${new}" ]; then
--- bin/suppress-outdated.sh
+++ bin/suppress-outdated.sh
@@ -3,13 +3,31 @@
 # Insert a package name to suppress in outdated checks.
 #
 
-package_name=$1
+case "$1" in
+  --major)
+    root=".outdatedSuppressions.major"
+    ;;
+  --minor)
+    root=".outdatedSuppressions.minor"
+    ;;
+  --patch)
+    root=".outdatedSuppressions"
+    ;;
+  *)
+    echo "suppress-outdated.sh: Unrecognized option: '$1'" 1>&2
+    echo "Usage: suppress-outdated.sh {--major|--minor|--patch} <package name>" 1>&2
+    exit 1
+    ;;
+esac
+
+package_name="$2"
 PACKAGE_JSON="package.json"
 
 if [ -z "$package_name" ]; then
-  echo "Usage: yarn suppress-outdated <package name>"
+  echo "Usage: yarn suppress-outdated {--major|--minor|--patch} <package name>"
   exit 1
 fi
+
 cp $PACKAGE_JSON $PACKAGE_JSON.old
-cat $PACKAGE_JSON | jq ".outdatedSuppressions.\"$package_name\" = (now | floor | . + 1209600)" > $PACKAGE_JSON.staging
+cat $PACKAGE_JSON | jq "${root}.\"${package_name}\" = (now | floor | . + 1209600)" > $PACKAGE_JSON.staging
 mv $PACKAGE_JSON.staging $PACKAGE_JSON
